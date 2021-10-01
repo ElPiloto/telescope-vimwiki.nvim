2124205ae1a4e0ffc414fb3c8873e694b0517fa1
Luis Piloto <luis.r.piloto@gmail.com>
2021-10-02 00:07:02 +0100

    Add vw "alias" for vimwiki in Telescope

---
+++ lua/telescope/_extensions/vw.lua
@@ -0,0 +1,11 @@
+--Allow short-hand 'vw' for vimwiki
+local vw_pickers = require'telescope._extensions.vimwiki.pickers'
+
+return require'telescope'.register_extension{
+  exports = {
+    vw = vw_pickers.vimwiki_pages,
+    live_grep = vw_pickers.vimwiki_grep,
+  }
+}
+
+
